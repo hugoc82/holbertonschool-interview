a17bcb9fa626825d67ff74a38e6404ed12b108fd
hugoc82 <hugocapelle@orange.fr>
2025-04-04 08:55:05 +0000

    .

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,8 +4,12 @@ def canUnlockAll(boxes):
     """
     Détermine si toutes les boîtes peuvent être ouvertes, en commençant par la boîte 0.
     
+    Cette fonction prend une liste de boîtes, où chaque boîte contient des clés pour ouvrir d'autres boîtes.
+    Le processus commence avec la boîte 0, qui est déjà déverrouillée, et explore les autres boîtes via les clés disponibles.
+    Si toutes les boîtes peuvent être ouvertes, la fonction retourne True, sinon elle retourne False.
+    
     Args:
-    boxes (list of list): Une liste de listes où chaque liste contient les clés pour d'autres boîtes.
+    boxes (list of list): Une liste de listes où chaque liste contient les indices des boîtes que l'on peut ouvrir avec les clés.
     
     Returns:
     bool: Retourne True si toutes les boîtes peuvent être ouvertes, sinon False.
