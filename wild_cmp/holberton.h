bdedc7c2a6826f00bbd360c051947ac09b9b6ca6
hugoc <hugocapelle@orange.fr>
2025-10-27 14:54:34 +0000

    .

--- wild_cmp/holberton.h
+++ wild_cmp/holberton.h
@@ -3,5 +3,9 @@
 
 int wildcmp(char *s1, char *s2);
 
+/* Helpers (prototypes demandés par l’énoncé) */
+char *advance_stars(char *s);
+int match_star(char *s1, char *s2);
+
 #endif /* HOLBERTON_H */
 
